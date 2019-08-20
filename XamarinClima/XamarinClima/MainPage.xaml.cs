f40cbd47ae5163523c976b0fbb31a77a48675c5b
NgeekReyes <ng_reyes@outlook.com>
2019-08-19 20:25:31 -0500

    ClimaApi_Xamarin
    consumi de api clima Xamarin

--- XamarinClima/XamarinClima/MainPage.xaml.cs
+++ XamarinClima/XamarinClima/MainPage.xaml.cs
@@ -5,17 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinClima.ViewModels;
+
 
 namespace XamarinClima
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            this.BindingContext = new MainPageViewModel();
         }
     }
 }
---
+++ XamarinClima/XamarinClima/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using XamarinClima.Models;
+
+namespace XamarinClima.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public WeatherData Data { get; set; }
+        public ICommand SearchCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            SearchCommand = new Command(async (searchTerm) =>
+            {
+                await GetData("https://api.darksky.net/forecast/362c425c81aca7a87cd0e0f47a7a556c/19.6863397,-99.0185679");
+            });
+        }
+
+        public async Task GetData(string url)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+
+            var response = await client.GetAsync(client.BaseAddress);
+
+            response.EnsureSuccessStatusCode();
+            var jsonResult = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<WeatherData>(jsonResult);
+            Data = result;
+
+        }
+    }   
+}
